0b3b8aa5da431013ca984035c4ebc62902fd0cba
krishnaclouds <prince.balakrishna@gmail.com>
2018-01-07 15:55:32 +0530

    Database Setup Basic files

--- flaskapp.py
+++ flaskapp.py
@@ -1,13 +1,24 @@
 from flask import Flask, request
+from flask_mysqldb import MySQL
 app = Flask(__name__, static_url_path='')
+
+app.config['MYSQL_HOST'] = 'localhost'
+app.config['MYSQL_USER'] = 'root'
+app.config['MYSQL_PASSWORD'] = 'murari'
+app.config['MYSQL_DB'] = 'BucketList'
+
+mysql = MySQL(app)
 
 @app.route('/')
 def root():
 	return app.send_static_file('index.html')
 
-@app.route('/somya')
-def somya():
-	return "World"
+@app.route('/db')
+def dbTest():
+	cur = mysql.connection.cursor()
+	cur.execute("select user_username from tbl_user where user_id=1")
+	rv = cur.fetchall()
+	return str(rv)
 
 if __name__ == '__main__':
-  app.run()
+  app.run(debug=True)
